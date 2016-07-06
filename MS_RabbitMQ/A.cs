94869492a7c0053dae21ad5151ea88ca1e6ab5b3
Meet Shah <meet.shah@aepl.com>
2016-07-06 14:56:46 +0530

    Initial Commit

---
+++ MS_RabbitMQ/A.cs
@@ -0,0 +1,29 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace MS_RabbitMQ
+{
+    public class A
+    {
+        public static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("tweets", false, false, false, null);
+
+                    string message = "Hi Rabbit";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish("", "hello", null, body);
+
+                    Console.WriteLine("{0} sent", message);
+                }
+            }
+        }
+
+    }
+}
